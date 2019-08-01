2567cb57391ee77068f0fc7093af021af34be9f6
Mateus Cruz Targino <ti.mateus@liga.org.br>
2019-08-01 12:47:03 -0300

    projeto de autenticacao

---
+++ auth/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { View} from "react-native";
+import firebase from "firebase";
+import { Header } from "./components/common";
+import LoginForm from "./components";
+
+class App extends Component {
+	componentDidMount() {
+		firebase.initializeApp({
+			apiKey: "AIzaSyA0lRhZEuaJLA6ohgCkaWfTLz3GsuwLqIQ",
+			authDomain: "auth-ff324.firebaseapp.com",
+			databaseURL: "https://auth-ff324.firebaseio.com",
+			projectId: "auth-ff324",
+			storageBucket: "",
+			messagingSenderId: "597783999280",
+			appId: "1:597783999280:web:01a3f457b3b7d2f6"
+		});
+	}
+	render() {
+		return (
+			<View>
+				<Header headerText="Authentication" />
+				<LoginForm />
+			</View>
+		);
+	}
+}
+
+export default App;
