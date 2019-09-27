07ce8a5816a835736d4d04537930d11fd2c37c4e
Mateus Cruz Targino <ti.mateus@liga.org.br>
2019-09-27 11:36:50 -0300

    state and reducers

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
+const reducer = (state, action) => {
+	switch (action.type) {
+		case "Increase":
+			return { ...state, count: state.counter + action.payload };
+		case "Decrease":
+			return { ...state, count: state.counter - action.payload };
+		default:
+			return state;
+	}
+};
 
 const CounterScreen = () => {
-	const [counter, setCounter] = useState(0);
-
+	const [state, dispatch] = useReducer(reducer, { count: 0 });
+	const { count } = state;
 	return (
 		<View>
-			<Button title="Increase" onPress={() => setCounter(counter + 1)} />
-			<Button title="Decrease" onPress={() => setCounter(counter - 1)} />
-			<Text>Current Count: {counter}</Text>
+			<Button
+				title="Increase"
+				onPress={() => dispatch({ type: "Increase", payload: 1 })}
+			/>
+			<Button
+				title="Decrease"
+				onPress={() => dispatch({ type: "Decrease", payload: 1 })}
+			/>
+			<Text>Current Count: {count}</Text>
 		</View>
 	);
 };
